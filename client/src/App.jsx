2d0dbfbb47eb8d1b69ae3a4235a01242bc58d049
robel tadesse <robeltadesse37@gmail.com>
2024-06-03 09:58:00 +1200

    setting the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import {Route, Routes, BrowserRouter} from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+import Profile from './Pages/Profile'
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={Home}></Route>
+        <Route path='/about' element={About}></Route>
+        <Route path='/Sign-in' element={Signin}></Route>
+        <Route path='/Sign-up' element={Signup}></Route>
+        <Route path='/profile' element={Profile}></Route>
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
