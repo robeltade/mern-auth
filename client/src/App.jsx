7f0a815ced4a134111596bdc160e406b6c98603f
robel tadesse <robeltadesse37@gmail.com>
2024-07-19 11:34:19 +1200

    setting home pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import About from './Pages/About'
 import Signin from './Pages/Signin'
 import Signup from './Pages/Signup'
 import Profile from './Pages/Profile'
+import Header from './Components/Header'
 
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path='/' element={Home}></Route>
-        <Route path='/about' element={About}></Route>
-        <Route path='/Sign-in' element={Signin}></Route>
-        <Route path='/Sign-up' element={Signup}></Route>
-        <Route path='/profile' element={Profile}></Route>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/Sign-in' element={<Signin/>}/>
+        <Route path='/Sign-up' element={<Signup/>}/>
+        <Route path='/profile' element={<Profile/>}/>
       </Routes>
     
     </BrowserRouter>
